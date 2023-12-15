1f873059fe1343259ed0f9b6a521dbbe13d02096
Gabriel <gabrielmaestre79@gmail.com>
2023-12-15 11:47:31 -0400

    Arreglado el tema del puerto

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { dbConnection } = require('./database/config')
 const cors = require('cors');
 
 console.log(process.env)
-
+const port = process.env.PORT || 4000
 // Crear el servidor de express
 
 
@@ -33,6 +33,6 @@ app.use('/api/events', require('./routes/events'))
 
 
 // Escuchar peticiones
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`)
 })
